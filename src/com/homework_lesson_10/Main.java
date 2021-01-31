fbf2111139cfa0eceb40cb6ed8ba3048afa6c570
TemptationWithin <75949800+TemptationWithin@users.noreply.github.com>
2021-01-31 20:25:23 +0300

    Add files via upload

---
+++ src/com/homework_lesson_10/Main.java
@@ -0,0 +1,77 @@
+package com.homework_lesson_10;
+
+import com.homework_lesson_10.myInterface.I_MathOperation;
+import java.util.Scanner;
+/**
+ * Создать обобщенный функциональный интерфейс
+ * Написать класс с методом мэйн.
+ * В этом методе мейн реализуйте логику
+ * если в консоль введена цифра 1, то:
+ *  использования интерфейса со строковым типом
+ *  и передать в метод интерфейса логику реверса строки(вывода строки в обратном порядке)
+ *
+ * если в консоль введена цифра 2, то:
+ * использования интерфейса с целочисленным типом и передать в метод интерфейса логику нахождения факториала числа
+ */
+public class Main{
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int index = scanner.nextInt();
+        scanner.close();
+
+        String testString = "Hello Andrew, here is my homework";
+        int testNumber = 7;
+
+        I_MathOperation i_mathOperation = null;
+        switch (index) {
+            case  (1):
+                i_mathOperation = new I_MathOperation<String>() {
+                    @Override
+                    public String select(String a) {
+                        return new StringBuilder(a).reverse().toString();
+                    }
+                };
+                break;
+            case (2):
+                i_mathOperation = new I_MathOperation<Integer>() {
+                    @Override
+                    public Integer select(Integer a) {
+                        int result = 1;
+                        for (int i = 1; i <= a; i++) {
+                            result = result*i;
+                        }
+                        return result;
+                    }
+                };
+                break;
+            default:
+                i_mathOperation = new I_MathOperation() {
+                    @Override
+                    public Object select(Object a) {
+                        return null;
+                    }
+                };
+                break;
+        }
+
+        if (index == 1){
+            System.out.println(i_mathOperation.select(testString));
+        } else if (index == 2){
+            System.out.println(i_mathOperation.select(testNumber));
+        } else System.out.println("unknown index");
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
---
+++ src/com/homework_lesson_10/myInterface/I_MathOperation.java
@@ -0,0 +1,6 @@
+package com.homework_lesson_10.myInterface;
+
+@FunctionalInterface
+public interface I_MathOperation<T> {
+    T select(T a);
+}
